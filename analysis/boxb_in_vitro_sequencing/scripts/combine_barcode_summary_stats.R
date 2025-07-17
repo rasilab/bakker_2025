712f1919a81dcd7a8b9348f437c6fd5a37bf91f5
rasi <rasi1983@gmail.com>
2025-07-17 12:05:20 -0700

    Correct summary stats script to include both target and loop parsing

--- analysis/boxb_in_vitro_sequencing/scripts/combine_barcode_summary_stats.R
+++ analysis/boxb_in_vitro_sequencing/scripts/combine_barcode_summary_stats.R
@@ -1,18 +1,16 @@
 options(warn = -1)
 
+suppressPackageStartupMessages(library(tidyverse))
 args <- commandArgs(trailingOnly = TRUE)
 
 summary_stats_folder <- args[1]
 output_file <- args[2]
 
-# summary_stats_folder <- "../data/summary_stats/58p1"
-
-suppressPackageStartupMessages(library(tidyverse))
 
 summary_stat_files <- list.files(path = summary_stats_folder, pattern = "csv", full.names = TRUE)
 
 summary_data <- read_csv(summary_stat_files, id = "filename", show_col_types = F) %>%
-  mutate(sample_id = str_extract(filename, "(?<=summary_stats/)[^/]+")) %>% 
+  mutate(sample_id = basename(dirname(filename))) %>% 
   mutate(barcode = str_extract(filename, "[^/]+(?=.csv)")) %>% 
   select(-filename) %>%
   select(sample_id, barcode, everything())
