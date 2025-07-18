4a405897421a161e99ad04eb47a931df1271b1fa
rasi <rasi1983@gmail.com>
2025-07-17 17:02:41 -0700

    Remove target tracking in boxb loop editing script

--- analysis/boxb_in_vitro_sequencing/scripts/calculate_boxb_loop_editing.py
+++ analysis/boxb_in_vitro_sequencing/scripts/calculate_boxb_loop_editing.py
@@ -1,110 +1,98 @@
 #!/usr/bin/env python
-# coding: utf-8
+"""
+Calculate BoxB loop editing statistics from FASTQ sequencing data.
 
-# ## Import Libraries
-
-# In[1]:
-
+This script processes paired-end FASTQ files to analyze BoxB editing patterns
+in specific loop regions, tracking UMI counts and nucleotide compositions.
+"""
 
 import pandas as pd
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
+from collections import defaultdict
 import itertools as it
 import sys
 
 
-# ## Define Paths to Files
-
-# In[18]:
-
-
-# barcode = 'AAGACGCCCA'
-# annotations_file = "../annotations/barcode_annotations.csv"
-# fastq_file = f"../data/split_fastq/i79_p1/{barcode}_R1.fastq"
-# fastq_umi = f"../data/split_fastq/i79_p1/{barcode}_R2.fastq"
-# output_file = f"../data/summary_stats/i79_p1/{barcode}.csv"
-
-umi_start = 0
-umi_length = 7
-target_edit_loc = [1,2,4,7,9,12,13,15]
-target_sequence = 'ATTATGGTGTAATTCTA'
-spacer3_boxb_start=60
-spacer5_boxb_start=12
-boxb_length= 19
-boxb_loop_loc=[7,9,10]
-
-annotations_file = sys.argv[1]
-fastq_file=sys.argv[2]
-fastq_umi=sys.argv[3]
-output_file= sys.argv[4]
-barcode = sys.argv[5]
-
-
-# ## Load Barcode Annotations and Subset to Those We Want to Process
-
-# In[19]:
-
-
-annotations = pd.read_csv(annotations_file).set_index('reverse_complement')
-
-annotations_barcode = annotations.loc[barcode]
-
-
-# ## Iterate through reads and parse the summary stats we want to collect
-
-# In[20]:
-
-
-R1_file = FastqGeneralIterator(fastq_file)
-R2_file = FastqGeneralIterator(fastq_umi)
-count_table = dict()
-umi_table = dict()
-for read1, read2  in zip(R1_file, R2_file):
-    umi = read2[umi_start:umi_length][1]
-    insert = read1[1][annotations_barcode['variable_start']:(annotations_barcode['variable_start'] + annotations_barcode['variable_length'])]
-    if annotations_barcode['oligo_name'].startswith("spacer3"):
-        boxb = read1[1][spacer3_boxb_start:spacer3_boxb_start+boxb_length]
-    elif annotations_barcode['oligo_name'].startswith("spacer5"):
-        boxb = read1[1][spacer5_boxb_start:spacer5_boxb_start+boxb_length]
-
-    if insert not in count_table:
-        count_table[insert] = {'umi_counts': 0}
-
-    if insert not in umi_table:
-        umi_table[insert] = dict()
-
-    if umi not in umi_table[insert]:
-        umi_table[insert][umi] = 1
-        count_table[insert]['umi_counts'] += 1
-    else:
-        umi_table[insert][umi] += 1
-        continue
-
-    boxb_loop = ''.join(boxb[loc] for loc in boxb_loop_loc)
-
-    for num,nt in it.product(range(len(boxb_loop) + 1), ['A','G','T','C','N']):
-        key = f'num_{num}_{nt}'
-        if  key not in count_table[insert]:
-          count_table[insert][key] = 0
+def initialize_count_dict(sequence_length):
+    """Initialize count dictionary with all possible keys."""
+    count_dict = {'umi_counts': 0}
+    nucleotides = ['A', 'G', 'T', 'C', 'N']
     
-    for nt in ['A','G','T','C','N']:
-        count = boxb_loop.count(nt)
-        count_table[insert][f"num_{count}_{nt}"] += 1
+    # Initialize nucleotide frequency counts
+    for num, nt in it.product(range(sequence_length + 1), nucleotides):
+        count_dict[f'num_{num}_{nt}'] = 0
+    
+    # Initialize position-specific counts
+    for pos, nt in it.product(range(sequence_length), nucleotides):
+        count_dict[f'pos_{pos}_{nt}'] = 0
+    
+    return count_dict
 
 
-    for num,nt in it.product(range(len(boxb_loop)), ['A','G','T','C','N']):
-        key = f'pos_{num}_{nt}'
-        if  key not in count_table[insert]:
-            count_table[insert][key] = 0
+def main():
+    # Parse command line arguments
+    annotations_file = sys.argv[1]
+    fastq_file = sys.argv[2]
+    fastq_umi = sys.argv[3]
+    output_file = sys.argv[4]
+    barcode = sys.argv[5]
+    
+    # Define constants
+    UMI_START = 0
+    UMI_LENGTH = 7
+    SPACER3_BOXB_START = 60
+    SPACER5_BOXB_START = 12
+    BOXB_LENGTH = 19
+    BOXB_LOOP_POSITIONS = [7, 9, 10]
+    NUCLEOTIDES = ['A', 'G', 'T', 'C', 'N']
+    SEQUENCE_LENGTH = len(BOXB_LOOP_POSITIONS)
+    
+    # Load barcode annotations
+    annotations = pd.read_csv(annotations_file).set_index('reverse_complement')
+    annotations_barcode = annotations.loc[barcode]
+    
+    # Determine BoxB extraction parameters
+    is_spacer3 = annotations_barcode['oligo_name'].startswith("spacer3")
+    boxb_start = SPACER3_BOXB_START if is_spacer3 else SPACER5_BOXB_START
+    boxb_end = boxb_start + BOXB_LENGTH
+    
+    # Initialize data structures
+    count_table = defaultdict(lambda: initialize_count_dict(SEQUENCE_LENGTH))
+    umi_sets = defaultdict(set)
+    
+    # Process paired FASTQ files
+    with open(fastq_file, 'r') as f1, open(fastq_umi, 'r') as f2:
+        R1_file = FastqGeneralIterator(f1)
+        R2_file = FastqGeneralIterator(f2)
+        
+        for read1, read2 in zip(R1_file, R2_file):
+            # Extract UMI and BoxB sequence
+            umi = read2[1][UMI_START:UMI_START + UMI_LENGTH]
+            boxb = read1[1][boxb_start:boxb_end]
+            
+            # Extract edited sequence from specific positions
+            boxb_edited_sequence = ''.join(boxb[pos] for pos in BOXB_LOOP_POSITIONS)
+            
+            # Skip if UMI already seen for this sequence
+            if umi in umi_sets[boxb_edited_sequence]:
+                continue
+            
+            umi_sets[boxb_edited_sequence].add(umi)
+            count_dict = count_table[boxb_edited_sequence]
+            count_dict['umi_counts'] += 1
+            
+            # Count nucleotides by frequency and position in single pass
+            for nt in NUCLEOTIDES:
+                count = boxb_edited_sequence.count(nt)
+                count_dict[f"num_{count}_{nt}"] += 1
+            
+            for pos, char in enumerate(boxb_edited_sequence):
+                count_dict[f'pos_{pos}_{char}'] += 1
+    
+    # Convert to DataFrame and save
+    count_table_df = pd.DataFrame.from_dict(dict(count_table), orient='index')
+    count_table_df.to_csv(output_file, index_label='boxb_edited_sequence')
 
-    for pos,char in enumerate(boxb_loop):
-        count_table[insert][f'pos_{pos}_{char}'] += 1
 
-
-# ## Write summary stats to output file
-
-# In[22]:
-
-
-count_table_df = pd.DataFrame.from_dict(count_table, orient='index')
-count_table_df.to_csv(output_file, index_label='insert')
-
+if __name__ == "__main__":
+    main()
